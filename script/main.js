afa008a97453c8c0fca2069daf330d5b39b93e30
Hollas <nicholasolajide@gmail.com>
2023-03-08 19:02:12 +0100

    desktop completion

---
+++ script/main.js
@@ -0,0 +1,23 @@
+const flexStyles = [
+  {
+    display: flex,
+    flexDirection: column,
+    alignItems: center,
+  },
+  {
+    display: flex,
+    flexDirection: column,
+    alignItems: center,
+    justifyContent: center
+  },
+  {
+    display: flex,
+    alignItems: center,
+    justifyContent: space-evenly
+  },
+  {
+    display: flex,
+    alignItems: center,
+    justifyContent: center
+  }
+]
