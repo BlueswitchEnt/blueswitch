0f85b75d63cf959e9889f525b49d6c324a86d441
Hollas <nicholasolajide@gmail.com>
2023-04-12 21:15:03 +0100

    features and events

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
-let searchIcon, searchBar;
+let searchIcon, searchBar, feat;
 searchIcon = document.getElementById("search-icon");
 searchBar = document.querySelector(".search-bar");
+feat = document.querySelector(".featy");
 
 // searchIcon.addEventListener("click", function() {
 //   searchBar.style.display = (searchBar.style.display === "none") ? "block" : "none";
@@ -8,4 +9,6 @@ searchBar = document.querySelector(".search-bar");
 
 searchIcon.addEventListener("click", function(){
   searchBar.classList.toggle("visible");
+  featy.classList.toggle("visible");
+
 });
